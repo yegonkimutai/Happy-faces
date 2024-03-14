9e199abd7cbe8f9c991ebe396bf74e558abaab39
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-03-14 12:28:50 +0300

    :building_construction: Add content to about and contact section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import NavScrollExample from './pages/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Projects from './pages/Projects';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
     </>
---
+++ src/pages/Contact.js
@@ -0,0 +1,51 @@
+import { AiOutlineMail } from 'react-icons/ai';
+
+import { BsWhatsapp } from 'react-icons/bs';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_nltl7gm', 'template_fsxpxkv', form.current, 'DMbJd6q03iyKE3cE7')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact" className="cont-main">
+      <h2>Contact Me</h2>
+
+      <div className="container contact-container">
+        <div className="contact-options">
+          <article className="contact-option">
+            <AiOutlineMail className="cont-icon" />
+            <h4>Email</h4>
+            <a href="mailto:yegonkimutai.99@gmail.com" target="_blank" rel="noreferrer">Send a message ✍️</a>
+          </article>
+          <article className="contact-option">
+            <BsWhatsapp className="cont-icon" />
+            <h4>WhatsApp</h4>
+            <a href="https://api.whatsapp.com/send?phone" target="_blank" rel="noreferrer">Send a message ✍️</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="name" name="name" placeholder="Full Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea name="message" placeholder="Message" rows="10" />
+          <button type="submit" className="btn primary">Send Message 📫 </button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
