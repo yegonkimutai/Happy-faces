52cbb2ddd30c654233134340c358c733318f94e5
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-03-19 13:44:48 +0300

    :art: Add icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,66 @@
 /* eslint-disable default-case */
-import React,  { useState }  from 'react'
-
-import image1 from '../images/img1.jpg'
-import image2 from '../images/img2.jpg'
-import image3 from '../images/img3.jpg'
-import image4 from '../images/img4.webp'
-import image5 from '../images/img5.png'
+import React from 'react'
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 import '../style/Home.css'
 
 export default function Home() {
-    const [count, setCount] = useState(0);
-    const [margin, setMargin] = useState('0%');
-    const [forward, setForward] = useState(true);
+  //   const [count, setCount] = useState(0);
+  //   const [margin, setMargin] = useState('0%');
+  //   const [forward, setForward] = useState(true);
 
-  const shift = (count) => {
-    switch (count) {
-      case 0:
-        setMargin('0%')
-        break;
-        case 1:
-        setMargin('-20%')
-        break;
-        case 2:
-        setMargin('-40%')
-        break;
-        case 3:
-        setMargin('-60%')
-        break;
-        case 4:
-        setMargin('-80%')
-        break;
-    } 
-  }
+  // const shift = (count) => {
+  //   switch (count) {
+  //     case 0:
+  //       setMargin('0%')
+  //       break;
+  //       case 1:
+  //       setMargin('-20%')
+  //       break;
+  //       case 2:
+  //       setMargin('-40%')
+  //       break;
+  //       case 3:
+  //       setMargin('-60%')
+  //       break;
+  //       case 4:
+  //       setMargin('-80%')
+  //       break;
+  //   } 
+  // }
 
-  if(forward) {
-    for(let i = 0; i < 5; i ++) {
-      setTimeout(() => {
-        if(count===5) {
-          setForward(false)
-        } else {
-          setCount(count + 1)
-          shift(count)
-        }
-      }, 2000)
-    }
-  } else {
-    for(let i = 5; i > 0; i --) {
-      setTimeout(() => {
-        if(count===0) {
-          setForward(true)
-        } else {
-          setCount(count - 1)
-          shift(count)
-        }
-      }, 1000)
-    }
-  }
+  // if(forward) {
+  //   for(let i = 0; i < 5; i ++) {
+  //     setTimeout(() => {
+  //       if(count===5) {
+  //         setForward(false)
+  //       } else {
+  //         setCount(count + 1)
+  //         shift(count)
+  //       }
+  //     }, 2000)
+  //   }
+  // } else {
+  //   for(let i = 5; i > 0; i --) {
+  //     setTimeout(() => {
+  //       if(count===0) {
+  //         setForward(true)
+  //       } else {
+  //         setCount(count - 1)
+  //         shift(count)
+  //       }
+  //     }, 1000)
+  //   }
+  // }
 
   return (
     <div className="app-home">
+    <div className='home-socials'>
+      <a href=''> <FaInstagram /> </a>
+      <a href=''> <FaTwitter /> </a>
+      <a href=''> <FaWhatsapp /> </a>
+    </div>
       <div className= 'car-cont'></div>
       <div className='content '>
       <h1>HAPPY FACES</h1>
