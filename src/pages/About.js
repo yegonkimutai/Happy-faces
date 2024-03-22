5f13089f346e79649d49e458e0d5f18e161becc4
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-03-22 20:14:36 +0300

    :children_crossing: Improve about and home styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,34 +1,35 @@
 import React from 'react'
 import '../style/About.css'
+import aboutimg from '../images/HPLogo.jpeg'
+
 
 function About() {
   return (
-    <div>
-        <h1 className='abt-title'>About</h1>
-        <p className='abt-desc'>
-        Happy Faces Empowerment Network started as a social welfare group back in 2017 by
-        young high schoolers who would visit children's homes, rescue centers, and vulnerability centers
-        during holidays. After the completion of high school, the group members decided to officially register the
-        organization with the government of Kenya on the 30th of March 2022. The initial exclusive
-        purpose during the organization's registration was to rescue vulnerable, orphaned, disabled, and
+   <section className='about'>
+      <div className='abt-img'>
+        <img src={aboutimg} alt=''/>
+      </div>
 
-        marginalized kids, situate them in our partner centers, and subsequently provide for their basic
-        needs like food and clothing while they are there. However, a Daily Nation report of 
-         <a href='https://nation.africa/kenya/news/orphanage-children-the-bane-of-turning-18--4217742' target='_blank' rel="noreferrer"> 30th March
-        2023 </a> 
-        highlighted the plight of these children once they get to the age of 18, where their contract with
-        the centers end, and most of them do not get to enjoy a smooth transition into normal lives.
-        Especially for the unsuccessful in their education, the males and females expressed finding
-        themselves to have engaged in crime, drug abuse, drug peddling, and prostitution among other
-        illegal acts. For a number, due to loss of hope, they sadly take their own lives. As a result, the
-        project will allow the youth to become self-dependent once they get to the age of forced transition
-        out of the children's homes and rescue centers that have been their homes for almost their whole
-        childhood lives. The project provides decent work opportunities for the vulnerable, orphaned,
-        disabled, and marginalized youth, assists in eradication of poverty across Kenyan society, and
-        minimizes the level of socio-economic inequalities faced by these vulnerable, orphaned, disabled,
-        and marginalized youth.
-        </p>
-    </div>
+      <div>
+      <h2 className='abt-title'>About</h2>
+      <p className='abt-desc'>
+          Happy Faces Empowerment Network started as a social welfare group back in 2017 by
+    young high schoolers who would visit children's homes, rescue centers, and vulnerability centers
+    during holidays. The initial exclusive purpose during the organization's registration was to rescue the kids in need, situate them in our partner centers, and subsequently provide for their basic
+    needs like food and clothing while they are there. However, a Daily Nation report of 
+    <a href='https://nation.africa/kenya/news/orphanage-children-the-bane-of-turning-18--4217742' target='_blank' rel="noreferrer"> 30th March
+    2023 </a> 
+    highlighted the plight of these children once they get to the age of 18, where their contract with
+    the centers end, and most of them do not get to enjoy a smooth transition into normal lives.
+  As a result, the project will allow the youth to become self-dependent once they get to the age of forced transition
+    out of the children's homes and rescue centers that have been their homes for almost their whole
+    childhood lives. The project provides decent work opportunities for the vulnerable, orphaned,
+    disabled, and marginalized youth, assists in eradication of poverty across Kenyan society, and
+    minimizes the level of socio-economic inequalities faced by these youths.
+      </p>
+      <button className='abt-btn'>Read More</button>
+      </div>
+   </section>
   )
 }
 
